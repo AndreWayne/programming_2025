a79a41068719ac85c6f316672137ccb639e3f0dc
AndreWayne <ivanchello757@gmail.com>
2025-09-26 00:48:49 +0300

    lab01

---
+++ lab_01/lab_01/lab_01.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+#include <limits>
+#include <locale.h>
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int x; //Катеты целочисленные
+    int y;
+    double z; //Гипотенуза может быть нецелым числом
+    double p;
+    std::cin >> x >> y;
+    z = sqrt(x * x + y * y);
+    p = x + y + z;
+    std::cout << "z=" << z << " " << "double" << " " << "p=" << p << " " << "double" << std::endl;
+    std::cout << "int, занимает " << sizeof(int) * 8 << "бит, мин. значение=" << std::numeric_limits<int>::min() << " макс. значение=" << std::numeric_limits<int>::max() << std::endl;
+    std::cout << "double, занимает " << sizeof(double) * 8 << "бит, мин. значение=" << std::numeric_limits<double>::min() << " макс. значение=" << std::numeric_limits<double>::max() << std::endl;
+}
+
